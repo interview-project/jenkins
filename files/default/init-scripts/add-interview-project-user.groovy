b5bbf74a1ce16e40c67ac0d44d44ee47cd6ebfe9
Ken Erwin <ken@devopslibrary.com>
2018-08-15 21:39:56 -0400

    Added user for nexus

--- files/default/init-scripts/add-interview-project-user.groovy
+++ files/default/init-scripts/add-interview-project-user.groovy
@@ -4,5 +4,6 @@ import com.cloudbees.plugins.credentials.domains.*;
 
 String keyfile = "/tmp/key"
 
-Credentials c = (Credentials) new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL,'9e271867-3858-42c8-823b-2fc4691bf223', "Github User", "interview-project-user", "interview-project-user1")
-SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), c)
+Credentials githubUser = (Credentials) new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL,'9e271867-3858-42c8-823b-2fc4691bf223', "Github User", "interview-project-user", "interview-project-user1")
+Credentials nexusUser = (Credentials) new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL,'9e271867-3858-42c8-823b-2fc4691bf223', "Nexus User", "admin", "admin123")
+SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), nexusUser)
